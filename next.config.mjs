8d2a3b686f2535fa61b2ecc4fc7c75c3694333be
senmu <senmu98@gmail.com>
2025-05-17 21:38:55 +0800

    feat: cloudflare 部署

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+ import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -10,5 +12,9 @@ const nextConfig = {
     unoptimized: true,
   },
 }
+
+ if (process.env.NODE_ENV === 'development') {
+   await setupDevPlatform();
+ }
 
 export default nextConfig
